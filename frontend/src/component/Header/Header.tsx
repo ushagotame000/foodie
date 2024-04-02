dbb2975f0445e19218a65bba413ea72e5a378162
Usha_Gotame <ushagotame000@gmail.com>
2024-04-02 07:46:51 +0545

    navbar updated

---
+++ frontend/src/component/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header h-[34vw] mx-9 auto  ">
+        <div className="header-content">
+          <h2>Order your favourite food here</h2>
+          <p>
+            Choose from a diverse menu hsodjgaojlkj agjajkagl gjaoijklasjg a
+          </p>
+          <button>Explore Menu</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
--- frontend/src/component/Navbar/Navbar.tsx
+++ frontend/src/component/Navbar/Navbar.tsx
@@ -1,10 +1,67 @@
-import React from "react";
-import logo from "../../assets/logo.png.png";
-import { FaCartShopping } from "react-icons/fa6";
+import React, { useState } from "react";
+// import { FaCartShopping } from "react-icons/fa6";
+import { assets } from "../../assets/assets";
 const Navbar = () => {
+  const [menu, setMenu] = useState("Home");
+
   return (
     <>
-      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+      <div className="navbar py-0 px-[20px] flex justify-between items-center">
+        <img src={assets.logo} alt="" className="logo w-20 " />
+        <ul className="navbar-menu flex list-none gap-12 decoration-gray-500 text-lg ">
+          <li
+            onClick={() => setMenu("Home")}
+            className={
+              menu === "Home"
+                ? "active border-b-2 border-solid border-gray-400 cursor-pointer"
+                : ""
+            }
+          >
+            Home
+          </li>
+          <li
+            onClick={() => setMenu("Menu")}
+            className={
+              menu === "Menu"
+                ? "active border-b-2 border-solid border-gray-400 cursor-pointer"
+                : ""
+            }
+          >
+            Menu
+          </li>
+          <li
+            onClick={() => setMenu("About")}
+            className={
+              menu === "About"
+                ? "active border-b-2 border-solid border-gray-400 cursor-pointer"
+                : ""
+            }
+          >
+            About
+          </li>
+          <li
+            onClick={() => setMenu("Contact")}
+            className={
+              menu === "Contact"
+                ? "active border-b-2 border-solid border-gray-400 cursor-pointer"
+                : ""
+            }
+          >
+            Contact
+          </li>
+        </ul>
+        <div className="navbar-right flex items-center gap-11">
+          <img src={assets.search_icon} alt="" />
+          <div className="navbar-search-icon relative">
+            <img src={assets.basket_icon} alt="" />
+          </div>
+          <button className="bg-gradient-to-r from-primary to-secondary text-white px-4 py-1 rounded-full hover:scale-105 duration-300 flex item-center gap-3 border-y-amber-800 transition ease-out hover:ease-in">
+            Sign IN
+          </button>
+        </div>
+      </div>
+
+      {/* <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
         <div className="container ">
           <div className="flex justify-between items-center">
             <div>
@@ -66,7 +123,7 @@ const Navbar = () => {
             </button>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
